113b7c0e7de90f1ec3489bc16b44c47860aaad22
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-09-19 18:20:56 +0300

    Add EventCatcherHawkular.ems_class pointing back to OpenShift EMS
    Not sure yet if it helped but this does get called from `sync_workers`.

--- app/models/manageiq/providers/openshift/container_manager/event_catcher_hawkular.rb
+++ app/models/manageiq/providers/openshift/container_manager/event_catcher_hawkular.rb
@@ -1,2 +1,6 @@
 class ManageIQ::Providers::Openshift::ContainerManager::EventCatcherHawkular < ManageIQ::Providers::Hawkular::MiddlewareManager::EventCatcher
+  def self.ems_class
+    #byebug_term
+    ManageIQ::Providers::Openshift::ContainerManager
+  end
 end
