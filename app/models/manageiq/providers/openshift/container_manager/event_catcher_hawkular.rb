cddccf90ed9695301b2ec6530783372220f40944
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-09-20 14:10:25 +0300

    Oops, Really don't inherit from middleware catcher

--- app/models/manageiq/providers/openshift/container_manager/event_catcher_hawkular.rb
+++ app/models/manageiq/providers/openshift/container_manager/event_catcher_hawkular.rb
@@ -1,4 +1,4 @@
-class ManageIQ::Providers::Openshift::ContainerManager::EventCatcherHawkular < ManageIQ::Providers::Hawkular::MiddlewareManager::EventCatcher
+class ManageIQ::Providers::Openshift::ContainerManager::EventCatcherHawkular < ManageIQ::Providers::BaseManager::EventCatcher
   require_nested :Runner
 
   def self.ems_class
