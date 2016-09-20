5340fd41ca4f49c63db295e7323e8817791bedbf
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-09-20 12:54:32 +0300

    Mutilate MiddlewareManager::EventCatcher.ems_class
    apparently workers get started for
    MiddlewareManager::EventCatcher
    *AND*
    ContainerManager::EventCatcherHawkular
    
    - The middleware workers then were killed by PerEmsWorkerMixin.sync_workers
      because there was no middleware EMS justifying their existance.
      This should fix that.
    - Now sync_workers still wants to kill some because
      "Duplicate workers found".

--- app/models/manageiq/providers/hawkular/middleware_manager/event_catcher.rb
+++ app/models/manageiq/providers/hawkular/middleware_manager/event_catcher.rb
@@ -1,3 +1,11 @@
 class ManageIQ::Providers::Hawkular::MiddlewareManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
   require_nested :Runner
+
+  # TODO: I'm trying to run ContainerManager::EventCatcherHawkular which inherits from this,
+  # but apparently this class gets instantiated ~~instead~~ too, and then killed by sync_workers
+  # because there are no Middleware providers.
+  def self.ems_class
+    #byebug_term
+    ManageIQ::Providers::Openshift::ContainerManager
+  end
 end
