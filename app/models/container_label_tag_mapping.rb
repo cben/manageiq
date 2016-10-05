76c3bb95707839e7629561cec71ce807fa2977e7
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-10-05 14:42:02 +0300

    Separate ContainerLabelTagMapping::Cached class
    Still using a single global cache, but will allow writing tests that
    reproduce multiple refresh workers having independent cache per process.
    
    Still in same file, needs stylistic cleanup - TBD.

--- app/models/container_label_tag_mapping.rb
+++ app/models/container_label_tag_mapping.rb
@@ -14,86 +14,101 @@ class ContainerLabelTagMapping < ApplicationRecord
   belongs_to :tag
 
   def self.drop_cache
-    @hash_all_by_name_type_value = nil
+    @global_cache = nil
   end
 
-  # Returns {[name, type, value] => [tag, ...]}}} hash.
-  def self.hash_all_by_name_type_value
-    unless @hash_all_by_name_type_value
-      @hash_all_by_name_type_value = {}
-      includes(:tag).find_each { |m| load_mapping_into_hash(m) }
-    end
-    @hash_all_by_name_type_value
-  end
-
-  def self.load_mapping_into_hash(mapping)
-    return unless @hash_all_by_name_type_value
-    key = [mapping.label_name, mapping.labeled_resource_type, mapping.label_value].freeze
-    @hash_all_by_name_type_value[key] ||= []
-    @hash_all_by_name_type_value[key] << mapping.tag
-  end
-  private_class_method :load_mapping_into_hash
-
-  # All specific-value tags that can be assigned by this mapping.
   def self.mappable_tags
-    hash_all_by_name_type_value.collect_concat do |(_name, _type, value), tags|
-      value ? tags : []
-    end
+    @global_cache ||= Cached.new
+    @global_cache.mappable_tags
   end
 
-  # Main entry point.
   def self.tags_for_entity(entity)
-    entity.labels.collect_concat { |label| tags_for_label(label) }
+    @global_cache ||= Cached.new
+    @global_cache.tags_for_entity(entity)
   end
 
   def self.tags_for_label(label)
-    # Apply both specific-type and any-type, independently.
-    (tags_for_name_type_value(label.name, label.resource_type, label.value) +
-     tags_for_name_type_value(label.name, nil,                 label.value))
+    @global_cache ||= Cached.new
+    @global_cache.tags_for_label(label)
   end
 
-  def self.tags_for_name_type_value(name, type, value)
-    specific_value = hash_all_by_name_type_value[[name, type, value]] || []
-    any_value      = hash_all_by_name_type_value[[name, type, nil]]   || []
-    if !specific_value.empty?
-      specific_value
-    else
-      any_value.map do |category_tag|
-        create_specific_value_mapping(name, type, value, category_tag).tag
+  class Cached
+    attr_reader :hash_all_by_name_type_value
+
+    # Builds {[name, type, value] => [tag, ...]}}} hash.
+    def initialize
+      @hash_all_by_name_type_value = {}
+      ContainerLabelTagMapping.includes(:tag).find_each { |m| load_mapping_into_hash(m) }
+    end
+
+    def load_mapping_into_hash(mapping)
+      return unless @hash_all_by_name_type_value
+      key = [mapping.label_name, mapping.labeled_resource_type, mapping.label_value].freeze
+      @hash_all_by_name_type_value[key] ||= []
+      @hash_all_by_name_type_value[key] << mapping.tag
+    end
+    private :load_mapping_into_hash
+
+    # All specific-value tags that can be assigned by this mapping.
+    def mappable_tags
+      hash_all_by_name_type_value.collect_concat do |(_name, _type, value), tags|
+        value ? tags : []
       end
     end
-  end
-  private_class_method :tags_for_name_type_value
 
-  # If this is an open ended any-value mapping, finds or creates a
-  # specific-value mapping to a specific tag.
-  def self.create_specific_value_mapping(name, type, value, category_tag)
-    new_tag = create_tag(name, value, category_tag)
-    new_mapping = create!(:labeled_resource_type => type, :label_name => name, :label_value => value,
-                          :tag => new_tag)
-    load_mapping_into_hash(new_mapping)
-    new_mapping
-  end
-  private_class_method :create_specific_value_mapping
-
-  def self.create_tag(name, value, category_tag)
-    category = category_tag.classification
-    unless category
-      category = Classification.create_category!(:description => "Kubernetes label '#{name}'",
-                                                 :read_only   => true,
-                                                 :tag         => category_tag)
+    # Main entry point.
+    def tags_for_entity(entity)
+      entity.labels.collect_concat { |label| tags_for_label(label) }
     end
 
-    if value.empty?
-      entry_name = ':empty:' # ':' character won't occur in kubernetes values.
-      description = '<empty value>'
-    else
-      entry_name = Classification.sanitize_name(value)
-      description = value
+    def tags_for_label(label)
+      # Apply both specific-type and any-type, independently.
+      (tags_for_name_type_value(label.name, label.resource_type, label.value) +
+       tags_for_name_type_value(label.name, nil,                 label.value))
     end
-    entry = category.add_entry(:name => entry_name, :description => description)
-    entry.save!
-    entry.tag
+
+    private
+
+    def tags_for_name_type_value(name, type, value)
+      specific_value = hash_all_by_name_type_value[[name, type, value]] || []
+      any_value      = hash_all_by_name_type_value[[name, type, nil]]   || []
+      if !specific_value.empty?
+        specific_value
+      else
+        any_value.map do |category_tag|
+          create_specific_value_mapping(name, type, value, category_tag).tag
+        end
+      end
+    end
+
+    # If this is an open ended any-value mapping, finds or creates a
+    # specific-value mapping to a specific tag.
+    def create_specific_value_mapping(name, type, value, category_tag)
+      new_tag = create_tag(name, value, category_tag)
+      new_mapping = ContainerLabelTagMapping.create!(:labeled_resource_type => type, :label_name => name, :label_value => value,
+                            :tag => new_tag)
+      load_mapping_into_hash(new_mapping)
+      new_mapping
+    end
+
+    def create_tag(name, value, category_tag)
+      category = category_tag.classification
+      unless category
+        category = Classification.create_category!(:description => "Kubernetes label '#{name}'",
+                                                   :read_only   => true,
+                                                   :tag         => category_tag)
+      end
+
+      if value.empty?
+        entry_name = ':empty:' # ':' character won't occur in kubernetes values.
+        description = '<empty value>'
+      else
+        entry_name = Classification.sanitize_name(value)
+        description = value
+      end
+      entry = category.add_entry(:name => entry_name, :description => description)
+      entry.save!
+      entry.tag
+    end
   end
-  private_class_method :create_tag
 end
